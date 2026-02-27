f4201efaedee4d7c42d17ce91d8780fb7c75a957
Lani Weisbart <>
2026-02-27 16:23:50 -0600

    Adds logic for calculating and creating participantEntryAmounts for percent-based split modes

--- src/main/java/com/expensesplitter/service/InstanceService.java
+++ src/main/java/com/expensesplitter/service/InstanceService.java
@@ -2,6 +2,7 @@ package com.expensesplitter.service;
 
 import com.expensesplitter.entity.*;
 import com.expensesplitter.enums.InstanceStatus;
+import com.expensesplitter.enums.SplitMode;
 import com.expensesplitter.repository.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,13 +18,16 @@ public class InstanceService {
     private final TemplateInstanceRepository instanceRepository;
     private final InstanceFieldValueRepository fieldValueRepository;
     private final TemplateService templateService;
+    private final ParticipantEntryAmountRepository participantEntryAmountRepository;
 
     public InstanceService(TemplateInstanceRepository instanceRepository,
                            InstanceFieldValueRepository fieldValueRepository,
-                           TemplateService templateService) {
+                           TemplateService templateService,
+                           ParticipantEntryAmountRepository participantEntryAmountRepository) {
         this.instanceRepository = instanceRepository;
         this.fieldValueRepository = fieldValueRepository;
         this.templateService = templateService;
+        this.participantEntryAmountRepository = participantEntryAmountRepository;
     }
 
     // Instance CRUD Operations
@@ -89,14 +93,20 @@ public class InstanceService {
         fieldValue.setAmount(amount);
         fieldValue.setNote(note);
         fieldValue.setEntryDate(entryDate);
-        fieldValue.setSplitMode(splitMode != null ? splitMode : com.expensesplitter.enums.SplitMode.DEFAULT);
+        fieldValue.setSplitMode(splitMode != null ? splitMode : SplitMode.DEFAULT);
 
         if (overrideSplitRuleId != null) {
             SplitRule overrideRule = templateService.getSplitRuleById(overrideSplitRuleId);
             fieldValue.setOverrideSplitRule(overrideRule);
         }
 
-        return fieldValueRepository.save(fieldValue);
+        InstanceFieldValue savedInstanceFieldValue = fieldValueRepository.save(fieldValue);
+
+        if(savedInstanceFieldValue.getSplitMode() != SplitMode.FIXED_AMOUNTS) {
+            calculateAndCreateParticipantEntryAmounts(savedInstanceFieldValue);
+        }
+
+        return savedInstanceFieldValue;
     }
 
     public List<InstanceFieldValue> getFieldValuesByInstance(UUID instanceId) {
@@ -132,5 +142,38 @@ public class InstanceService {
 
     public void deleteFieldValue(UUID fieldValueId) {
         fieldValueRepository.deleteById(fieldValueId);
+    }
+
+    // Calculation & Auto-Allocation
+    public void calculateAndCreateParticipantEntryAmounts(InstanceFieldValue fieldValue) {
+
+        // Determine which split rule to use
+        SplitRule splitRule = null;
+        if (fieldValue.getOverrideSplitRule() != null) {
+            splitRule = fieldValue.getOverrideSplitRule();
+        } else {
+            splitRule = fieldValue.getTemplateField().getDefaultSplitRule();
+        }
+
+        // If no split rule is defined, we can't allocate
+        if (splitRule == null) {
+            throw new RuntimeException("No split rule defined for field value: " + fieldValue.getId());
+        }
+
+        // Get all allocations (percentages) for this split rule
+        List<SplitRuleAllocation> ruleAllocations = templateService.getAllocationsForRule(splitRule.getId());
+
+        // Create a participant_entry_amount for each person in the split rule
+        for (SplitRuleAllocation ruleAllocation : ruleAllocations) {
+            BigDecimal participantAmount = fieldValue.getAmount()
+                    .multiply(ruleAllocation.getPercent())
+                    .divide(new BigDecimal("100"), 2, java.math.RoundingMode.HALF_UP);
+
+            ParticipantEntryAmount participantEntryAmount = new ParticipantEntryAmount();
+            participantEntryAmount.setInstanceFieldValue(fieldValue);
+            participantEntryAmount.setTemplateParticipant(ruleAllocation.getTemplateParticipant());
+            participantEntryAmount.setAmount(participantAmount);
+            participantEntryAmountRepository.save(participantEntryAmount);
+        }
     }
 }
